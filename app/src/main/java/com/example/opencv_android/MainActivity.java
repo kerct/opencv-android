739ddd2dc9db80b9ca1932b302f7eabfaeb9767b
Chin Tian <kerchintian@gmail.com>
2019-04-03 14:00:07 +0800

    shows opencv camera

--- app/src/main/java/com/example/opencv_android/MainActivity.java
+++ app/src/main/java/com/example/opencv_android/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.opencv_android;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
-    private final String TAG = "MainActivity";
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String TAG = "MainActivity";
+    private final int PERMISSION_REQUEST_CAMERA = 1;
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+    Mat mat1, mat2, mat3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,149 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             Log.e(TAG, "OpenCV not loaded");
+        }
+
+        if(!allPermissionsGranted()) {
+            getCameraPermission();
+        }
+        else {
+            showCamera();
+        }
+    }
+
+    private void showCamera() {
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    showCamera();
+                } else {
+                    // permission denied
+                }
+                return;
+            }
+        }
+    }
+
+    private static boolean isPermissionGranted(Context context, String permission) {
+        if (ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission granted: " + permission);
+            return true;
+        }
+        Log.i(TAG, "Permission NOT granted: " + permission);
+        return false;
+    }
+
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info =
+                    this.getPackageManager()
+                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+            if (ps != null && ps.length > 0) {
+                return ps;
+            } else {
+                return new String[0];
+            }
+        } catch (Exception e) {
+            return new String[0];
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void getCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CAMERA);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mat1 = new Mat(width, height, CvType.CV_8UC4);
+        mat2 = new Mat(width, height, CvType.CV_8UC4);
+        mat3 = new Mat(width, height, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mat1.release();
+        mat2.release();
+        mat3.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mat1 = inputFrame.rgba();
+
+        //TODO rotate the frame by 90 degrees
+        Core.transpose(mat1, mat2);
+        Imgproc.resize(mat2, mat3, mat3.size(), 0, 0, 0);
+        Core.flip();
+
+        return mat1;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "Error initialising OpenCV", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
         }
     }
 }
